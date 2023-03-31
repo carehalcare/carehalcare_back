afbf0897280f4c1cc2b724b413f9386077efd296
Ghoi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-03-31 14:09:32 +0900

    init

---
+++ src/main/java/carehalcare/carehalcare/CarehalcareApplication.java
@@ -0,0 +1,13 @@
+package carehalcare.carehalcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarehalcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarehalcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carehalcare/carehalcare/CarehalcareApplicationTests.java
@@ -0,0 +1,13 @@
+package carehalcare.carehalcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarehalcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
