2b60a449e817da0a83f64afbbfdb7e92009c18c5
Ghoi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-04-02 17:16:58 +0900

    docs: apply Swagger UI

--- src/main/java/carehalcare/carehalcare/controller/board/AdministrationController.java
+++ src/main/java/carehalcare/carehalcare/controller/board/AdministrationController.java
@@ -4,11 +4,16 @@ import carehalcare.carehalcare.dto.board.administraion.AdministrationListRespons
 import carehalcare.carehalcare.dto.board.administraion.AdministrationResponseRequestDto;
 import carehalcare.carehalcare.dto.board.administraion.AdministrationSaveRequestDto;
 import carehalcare.carehalcare.service.board.AdministrationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags="투약 기록 API")
 @CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RestController
@@ -17,12 +22,18 @@ public class AdministrationController {
     private final AdministrationService administrationService;
 
     /* 투약 기록 저장 */
+    @ApiOperation(value="투약 기록 저장", notes="puserId는 보호자 아이디, userId는 간병인 아이디")
     @PostMapping("/administrations")
     public Long save(@RequestBody AdministrationSaveRequestDto requestDto){
         return administrationService.save(requestDto);
     }
 
     /* 투약 기록 리스트 조회 */
+    @ApiOperation(value="투약 기록 리스트 조회")
+    @ApiImplicitParams(
+            {@ApiImplicitParam(name = "uid", value = "간병인 아이디"),
+                    @ApiImplicitParam(name = "puid", value = "보호자 아이디")}
+    )
     @GetMapping("/administrations/list/{uid}/{puid}")
     public List<AdministrationListResponseRequestDto> list(
             @PathVariable("uid") String userId,
@@ -31,6 +42,8 @@ public class AdministrationController {
     }
 
     /* 투약 기록 상세 조회 */
+    @ApiOperation(value="투약 기록 상세 조회")
+    @ApiImplicitParam(name = "id", value = "게시글 아이디")
     @GetMapping("/administrations/{id}")
     public AdministrationResponseRequestDto findById(
             @PathVariable("id") Long id) throws Exception{
