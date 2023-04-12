e61a87e40282433ff85f9e101b51a58808e11f6c
Ghoi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-04-12 15:23:04 +0900

    docs: set utf-8 encoding in swagger

--- src/main/java/carehalcare/carehalcare/config/SwaggerConfig.java
+++ src/main/java/carehalcare/carehalcare/config/SwaggerConfig.java
@@ -10,6 +10,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -29,5 +32,21 @@ public class SwaggerConfig {
                 .version("0.0.1")
                 .description("CarehalCare API")
                 .build();
+    }
+
+    private Set<String> getConsumeContentTypes(){
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        consumes.add("multipart/form-data");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes(){
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        produces.add("application/x-www-form-urlencoded");
+        produces.add("multipart/form-data");
+        return produces;
     }
 }
