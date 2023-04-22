0437d088a865e76e5f0f80861be8a22120d44aaf
Ghoi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-04-22 20:33:27 +0900

    fix: Meal Image Response Dto

--- src/main/java/carehalcare/carehalcare/dto/board/meal/MealImageResponseDto.java
+++ src/main/java/carehalcare/carehalcare/dto/board/meal/MealImageResponseDto.java
@@ -3,6 +3,13 @@ package carehalcare.carehalcare.dto.board.meal;
 import carehalcare.carehalcare.domain.board.meal.MealImage;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
 
 @Getter
 @NoArgsConstructor
@@ -12,6 +19,7 @@ public class MealImageResponseDto {
     private String storeFilename;
     private String filePath;
     private Long mealId;
+    private String encodedString;
 
 
     public MealImageResponseDto(MealImage entity){
@@ -20,6 +28,15 @@ public class MealImageResponseDto {
         this.storeFilename=entity.getStoreFilename();
         this.filePath=entity.getFilePath();
         this.mealId=entity.getMeal().getId();
+
+        try{
+            InputStream inputStream = new FileInputStream(this.filePath);
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            this.encodedString = Base64.getEncoder().encodeToString(bytes);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
 }
