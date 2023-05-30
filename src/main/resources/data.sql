5f5881e84076b0b7bda8e09892ababf9e4352c2f
Choi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-05-30 21:06:27 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO USER_TABLE (BIRTH_DATE, CODE, CUSER_ID, PASSWORD, PHONE, PUSER_ID, SEX, USER_ID, USERNAME)
- VALUES ('1999-08-02', 1, '', 'password1', '010-0000-0000', '', 'F', 'userid1', '보호자'),
-    ('1998-01-15', 0, '', 'password2', '010-1111-0000', '', 'M', 'userid2', '간병인');
+ VALUES ('1979-01-15', 1, '', 'password1', '010-0000-0000', '', 'M', 'userid1', '이주연'),
+    ('1971-04-11', 0, '', 'password2', '010-1111-0000', '', 'F', 'userid2', '유지민');
 
 
 
